faf28db20e3728cae27928d4d563a49aa2ac4ede
kidaqrusFan01 <ambaololo@gmail.com>
2026-06-24 12:31:33 -0700

    changed to look amazon like

--- corazon/urls.py
+++ corazon/urls.py
@@ -1,8 +1,12 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+
+    # AUTH ROUTES - this makes login/logout work
+    path('accounts/', include('django.contrib.auth.urls')),
+
 
     path(
     'add-product/',

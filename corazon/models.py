407b970b25282a47f183722407b5f67068f78e5c
kidaqrusFan01 <ambaololo@gmail.com>
2026-06-29 08:04:02 -0700

    added category to be saved

--- corazon/models.py
+++ corazon/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.db.models import Avg
+from django.utils.text import slugify
 
 
 # Create your models here.
@@ -14,6 +15,11 @@ class Category(models.Model):
         blank=True,
         null=True
     )
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
+
 
     def __str__(self):
         return self.name
